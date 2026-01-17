3aed40e2c1f222e9bf3ca834384a21d68f4a093f
jackson97010 <jackson97010@yahoo.com.tw>
2026-01-17 15:58:44 +0800

    push

--- _replay_system/decode.py
+++ _replay_system/decode.py
@@ -7,12 +7,12 @@ import os
 # 1. 設定檔案與路徑
 # ==========================================
 # 輸入檔案路徑
-file_path = r"C:\Users\User\Documents\_web\_01_web\data\single_query_results\2025-12-29\8074.parquet"
+file_path = r"C:\Users\User\Documents\_02_bt\Backtest_tick_module\data\raw\20260109\6443.parquet"
 
 
 # 輸出檔案名稱
 output_dir = ""  # 留空表示輸出到當前目錄，或設為 "app" 輸出到 app 子目錄
-output_filename = "8074_1229.json"
+output_filename = "6443_0109.json"
 output_json = os.path.join(output_dir, output_filename) if output_dir else output_filename
 
 if not os.path.exists(file_path):
